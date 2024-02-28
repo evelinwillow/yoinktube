03048766d4a8e97628a0e05c2cfb1e789e4e61e7
evelinwillow <me@evelin.pink>
2024-02-28 21:46:57 +0100

    small fix

--- src/include/post.h
+++ src/include/post.h
@@ -3,15 +3,19 @@
 #ifndef YOINKTUBE_POST_H
 #define YOINKTUBE_POST_H
 
-struct yoinktube_request_response {
+struct yoinktube_request_response
+{
   char *memory;
   size_t size;
 };
 
-struct yoinktube_request_params {
+struct yoinktube_request_params
+{
   char *url;
   char *body;
   struct yoinktube_request_response *response;
 };
+
+int yoinktube_request ( struct yoinktube_request_params );
 
 #endif
