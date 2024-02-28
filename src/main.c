d1aeef81dbf7596e7fab702927d026acf839d494
evelinwillow <me@evelin.pink>
2024-02-28 21:42:19 +0100

    implemented sigint handler

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
+#include <signal.h>
+
+#include "post.c"
 
 int main ( int argc, char *argv[] )
 {
 	( void ) argc;
 	( void ) argv;
+
+	signal ( SIGINT, yoinktube_sigint_handler );
 
 	abort();
 }
