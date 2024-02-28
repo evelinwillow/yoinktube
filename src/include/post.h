dc5799a105ff0dce7c47a3c65f1d3d3df3f44fd0
evelinwillow <me@evelin.pink>
2024-02-28 21:17:27 +0100

    added yoinktube_request_responses struct

--- src/include/post.h
+++ src/include/post.h
@@ -1,10 +1,17 @@
+#include <stdio.h>
+
 #ifndef YOINKTUBE_POST_H
 #define YOINKTUBE_POST_H
+
+struct yoinktube_request_response {
+  char *memory;
+  size_t size;
+};
 
 struct yoinktube_request_params {
   char *url;
   char *body;
-  char *response;
+  struct yoinktube_request_response *response;
 };
 
 #endif
